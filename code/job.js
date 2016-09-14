a4c863d7c53967cc520793e713873d15dc66e23c
Azat Mardan <hi@azat.co>
2016-09-14 13:31:08 +0200

    add changes :atm:

--- code/job.js
+++ code/job.js
@@ -2,17 +2,19 @@
 var util = require('util')
 var events = require('events')
 var Job = function Job() {
-  var job = this // We'll use it later to pass 'this' to closures
+  var job = this
+  // We'll use it later to pass 'this' to closures
   // ...
   job.process = function() {
     // ...
-    setTimeout(function(){ // Emulate the delay of the job - async!
+    setTimeout(function(){
+      // Emulate the delay of the job - async!
       job.emit('done', { completedOn: new Date() })
     }, 700)
   }
-  // job.on('start', function(){
-  //   job.process()
-  // })
+  job.on('start', function(){
+    job.process()
+  })
 }
 
 util.inherits(Job, events.EventEmitter)
--- code/weekly.js
+++ code/weekly.js
@@ -3,10 +3,11 @@ var Job = require('./job.js')
 var job = new Job()
 
 job.on('done', function(details){
-  console.log('Weekly email job was completed at', details.completedOn)
+  console.log('Weekly email job was completed at',
+    details.completedOn)
   // job.removeAllListeners()
 
 })
 
-job.process()
-// job.emit('start')
+// job.process()
+job.emit('start')
