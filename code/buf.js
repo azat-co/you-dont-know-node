845cd06321d3a9cb1969fdc75810eb909dab5c47
Azat Mardan <hi@azat.co>
2016-09-14 13:42:34 +0200

    add changes :heart:

--- code/buf.js
+++ code/buf.js
@@ -2,5 +2,7 @@ var buf = new Buffer(26)
 for (var i = 0 ; i < 26 ; i++) {
   buf[i] = i + 97 // 97 is ASCII a
 }
-console.log(buf) // <Buffer 61 62 63 64 65 66 67 68 69 6a 6b 6c 6d 6e 6f 70 71 72 73 74 75 76 77 78 79 7a>
-console.log(buf.toString('utf8')) // abcdefghijklmnopqrstuvwxyz
+console.log(buf)
+// <Buffer 61 62 63 64 65 66 67 68 69 6a 6b 6c 6d 6e 6f 70 71 72 73 74 75 76 77 78 79 7a>
+console.log(buf.toString('utf8'))
+// abcdefghijklmnopqrstuvwxyz
